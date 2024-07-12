9ba12a03cae9de7314ac362c748690db320cd432
ArtyomIzmaylov <artyomguybov2002@gmail.com>
2024-07-13 02:54:27 +0300

    .env was added

--- src/client/json-placeholder/json-placeholder.ts
+++ src/client/json-placeholder/json-placeholder.ts
@@ -1,11 +1,12 @@
 import {JsonPlaceholderInterface} from "./json-placeholder.interface.ts";
 import {GetCategoriesRequestInterface, GetCategoriesResponseInterface} from "./model/get-categories.ts";
+import axios from "axios";
 
 
 export class JsonPlaceholder implements JsonPlaceholderInterface{
 
 
     getPosts(req: GetCategoriesRequestInterface): Promise<GetCategoriesResponseInterface> {
-
+        return axios.get("")
     }
 }
